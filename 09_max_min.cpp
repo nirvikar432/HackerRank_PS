a621803008c39e82777cbfa3daaaecb8dcabedc9
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-19 22:55:28 +0530

    Small Change in Question 09

--- 09_max_min.cpp
+++ 09_max_min.cpp
@@ -27,3 +27,28 @@ int main()
     }        
     cout<<small<<" "<<great;
 }
+
+/* another way
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int arr[5],sum=0,great=0,small=INT_MAX;
+    for(int i = 0; i<5; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    for(int i = 0; i<5; i++)
+        sum +=arr[i];
+        
+    for(int i=0;i<5;i++)
+    {
+        great = max(great, arr[i]);
+        small = min(small, arr[i]);
+    }
+    cout<<sum-great<<" "<<sum-small;
+}
+
+
+*/
