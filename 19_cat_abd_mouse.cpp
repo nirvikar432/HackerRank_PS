a30e176c52c57ae93ec3888e20e86a73ccc623d5
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-24 00:33:52 +0530

    2nd way in Q19

--- 19_cat_abd_mouse.cpp
+++ 19_cat_abd_mouse.cpp
@@ -25,3 +25,31 @@ int main()
     }
 
 }
+
+/*
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int N;
+    cin>>N;
+    int arr[100];
+    for(int i=0;i<(3*N);i++)
+    {
+        cin>>arr[i];
+    }
+    for(int i=0;i<(3*N);i=i+3)
+    {
+        int a,b;
+        a=abs(arr[i]-arr[i+2]);
+        b=abs(arr[i+1]-arr[i+2]);
+        if(a>b)
+        cout<<"Cat B"<<endl;
+        else if(b>a)
+        cout<<"Cat A"<<endl;
+        else
+        cout<<"Mouse C"<<endl;
+    }
+    return 0;
+}
+*/
