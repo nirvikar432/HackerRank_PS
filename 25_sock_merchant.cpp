8c65f378e9b61f922f622ec12a27112d567adbe3
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-28 00:04:19 +0530

    Solved

--- 25_sock_merchant.cpp
+++ 25_sock_merchant.cpp
@@ -4,23 +4,16 @@
 using namespace std;
 int main()
 {
-    int n,arr[101],flag[101],i,count=0;
+    int n,c,freq[101]={},i,count=0;
     cin>>n;
+    for(i=0;i<n;i++)
+    {
+        cin>>c;
+        freq[c]++;
+    }
     for(i=0;i<101;i++)
     {
-        flag[i]=0;
-    }
-    for(i=0;i<n;i++)
-    {
-        cin>>arr[i];
-    }
-    for(i=0;i<n;i++)
-    {
-        flag[arr[i]]++;
-    }
-    for(i=1;i<101;i++)
-    {
-        count+=flag[i]/2;
+        count+=freq[i]/2;
     }
     cout<<count;
 
