7b1c67ad2ff3dc7151b1423dc247c0c8e46205d6
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-27 23:53:59 +0530

    Solved

---
+++ 25_sock_merchant.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,arr[101],flag[101],i,count=0;
+    cin>>n;
+    for(i=0;i<101;i++)
+    {
+        flag[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        flag[arr[i]]++;
+    }
+    for(i=1;i<101;i++)
+    {
+        count+=flag[i]/2;
+    }
+    cout<<count;
+
+}
