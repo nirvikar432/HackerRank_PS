ce6750c32a536b84645f72474d38a8d436587c5f
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-28 22:23:47 +0530

    Indent

--- 26_drawing_book.cpp
+++ 26_drawing_book.cpp
@@ -1,11 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n,k;
-    
-    cin>>n>>k;
-    if (n%2 == 0)
-		n++;
-	cout<< min((k)/2, (n - k)/2);
+  int n, k;
+  cin >> n >> k;
+  if (n % 2 == 0)
+  n++;
+  cout << min((k) / 2, (n - k) / 2);
 }
