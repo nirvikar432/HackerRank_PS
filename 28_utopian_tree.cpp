afaf7abf8078b6fff8f2a9aff01ffb57e2d44b22
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-29 14:55:15 +0530

    Indent

--- 28_utopian_tree.cpp
+++ 28_utopian_tree.cpp
@@ -1,25 +1,23 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
-    int n,m,ht=1;
-    cin>>n;
-    for(int i=0;i<n;i++,ht=1)
+    int n, m, ht = 1;
+    cin >> n;
+    for (int i = 0; i < n; i++, ht = 1)
     {
-        cin>>m;
-        for(int j=1;j<=m;j++)
+        cin >> m;
+        for (int j = 1; j <= m; j++)
         {
-            if(j%2!=0)
+            if (j % 2 != 0)
             {
-            ht*=2;
+                ht *= 2;
             }
-            else if(j%2==0)
+            else if (j % 2 == 0)
             {
-            ht+=1;
+                ht += 1;
             }
         }
-        cout<<ht<<endl;
-        
+        cout << ht << endl;
     }
-    
 }
