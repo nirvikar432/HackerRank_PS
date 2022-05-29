9ef9bdc545ce5581e8edbb7220e3c4bdc0925a1d
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-29 14:44:49 +0530

    Solved

---
+++ 28_utopian_tree.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m,ht=1;
+    cin>>n;
+    for(int i=0;i<n;i++,ht=1)
+    {
+        cin>>m;
+        for(int j=1;j<=m;j++)
+        {
+            if(j%2!=0)
+            {
+            ht*=2;
+            }
+            else
+            {
+            ht+=1;
+            }
+        }
+        cout<<ht<<endl;
+    }
+}
