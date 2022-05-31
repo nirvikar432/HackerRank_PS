507af16a6d32899bb0199d4e0d349f10f55ee369
NIRVIKAR <nirvikarprasoon@gmail.com>
2022-05-31 22:32:53 +0530

    Solved

--- 32_the_birthday_bar.cpp
+++ 32_the_birthday_bar.cpp
@@ -10,16 +10,16 @@ int main()
         cin>>arr[i];
     }
     cin>>d>>m;
-    for(i=m;i<=n;i++)
+    for(i=1;i<=n;i++)
     {
         int sum=0;
-        for(j=k;j<=i;j++)
+        for(j=i;j<=m+i-1;j++)//if i=1 and m=2 then j = 2 in arr 1 2 1 3 2
         {
             sum+=arr[j];
         }
         if(sum==d)
         flag++;
-        k++;
+        //k++;
     }
     cout<<flag;
 }
